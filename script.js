227c07a2f9cbf90e00f93d0f3e6eb5616da41070
Claudio Junior <82058070+Claudio-16dv@users.noreply.github.com>
2022-09-20 16:58:07 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,29 @@
+async function buscaEndereco(cep) {
+    var mensagemDeErro = document.getElementById('erro');
+    mensagemDeErro.innerHTML = "";
+    try{
+        var consultaCep = await fetch(`https://viacep.com.br/ws/${cep}/json/`);
+        var consultaCepConvertido = await consultaCep.json();
+        if (consultaCepConvertido.erro) {
+            throw Error ('CEP não existente!');
+        }
+        var cidade = document.getElementById('cidade');
+        var logradouro = document.getElementById('endereco');
+        var estado = document.getElementById('estado');
+        var bairro = document.getElementById('bairro')
+
+        cidade.value = consultaCepConvertido.localidade;
+        logradouro.value = consultaCepConvertido.logradouro;
+        estado.value = consultaCepConvertido.uf;
+        bairro.value = consultaCepConvertido.bairro;
+
+        console.log(consultaCepConvertido);
+        return consultaCepConvertido;
+    } catch(erro) {
+        mensagemDeErro.innerHTML = `<p>CEP inválido. Tente novamente!</p>`;
+        console.log(erro);
+    }
+};
+
+var cep = document.getElementById('cep');
+cep.addEventListener("focusout", () => buscaEndereco(cep.value));
